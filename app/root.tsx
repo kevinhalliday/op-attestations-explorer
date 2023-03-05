adc25a921a0ee8f651f22a3e66c1f6a13cf46a8f
Kevin Halliday <halliday.kevin7@gmail.com>
2023-03-05 15:35:40 -0500

    Add open graph meta and apple-touch-icon link.

--- app/root.tsx
+++ app/root.tsx
@@ -14,11 +14,15 @@ export const meta: MetaFunction = () => ({
   title: 'OP Attestations Explorer',
   description: 'Explore attestations on Optimism. Search by key or address.',
   viewport: 'width=device-width,initial-scale=1',
+  'og:title': 'OP Attestations Explorer',
+  'og:description': 'Explore attestations on Optimism. Search by key or address.',
+  'og:image': '/attestation-explorer-preview.png',
 })
 
 export const links: LinksFunction = () => [
   { rel: 'stylesheet', href: stylesheet },
   { rel: 'icon', href: '/favicon.png' },
+  { rel: 'apple-touch-icon', href: '/favicon.png' },
 ]
 
 export default function App() {
